99a8f0df3e5ffa641e2b985b56e5d72c546fe453
Kevin Simms <ksimms2@cnm.edu>
2019-02-04 14:15:52 -0700

    Implemented tallies. Unit tests for Game.

--- src/edu/cnm/deepdive/craps/model/Game.java
+++ src/edu/cnm/deepdive/craps/model/Game.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.craps.model;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -10,6 +11,8 @@ public class Game {
   private State state;
   private Random rng;
   private List<int[]> rolls;
+  private long wins;
+  private long losses;
 
   public Game(Random rng){ // constructor
     this.rng = rng;
@@ -38,6 +41,11 @@ public class Game {
   public State play(){
     while (state != State.WIN && state != State.LOSS){
       roll();
+      if (state == State.WIN) {
+        wins++;
+      }else if (state == State.LOSS) {
+        losses++;
+      }
     }
     return state;
   }
@@ -51,7 +59,18 @@ public class Game {
   }
 
   public List<int[]> getRolls() {
-    return new LinkedList<int[]>(rolls);
+    List<int[]> copy = new LinkedList<int[]>();
+    for (int[] roll : rolls){
+      copy.add(Arrays.copyOf(roll, roll.length));
+    }
+    return copy;
   }
 
+  public long getWins() {
+    return wins;
+  }
+
+  public long getLosses() {
+    return losses;
+  }
 }
